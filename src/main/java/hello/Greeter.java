5255ac64abe0100f3e9f265e59d6143f041e7e75
anirudhadak2 <123630876+anirudhadak2@users.noreply.github.com>
2023-07-10 07:21:33 +0530

    Create Greeter.java

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class Greeter {
+  public String sayHello() {
+    return "Hello WORLD welcome to unnati !! Hi and Bye  hello is the string needed here. testig for CKACLASS for webhooks";
+  }
+}

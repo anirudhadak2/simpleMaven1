b7f47dfd66fa3c9cbe48acf8f8a83030fe0694fc
anirudhadak2 <123630876+anirudhadak2@users.noreply.github.com>
2023-07-19 12:21:10 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello Anirudha here !! Hi  ALL THIS GOING WELL. deploying java code on jenkins using maven plugins  !!!! Bye ";
+    return "Hello Anirudha here !! Hi  ALL THIS GOING WELL.   !!!! Bye ";
   }
 }

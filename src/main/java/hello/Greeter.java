e3b4ee21f1c34dcb6039fa4823cb694f417c6971
anirudhadak2 <123630876+anirudhadak2@users.noreply.github.com>
2023-07-19 11:40:52 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello Anirudha here !! Hi  ALL THIS GOING WELL. jenkins will done through the maven plugins CICD  WORKING VERY WELL  !!!! Bye ";
+    return "Hello Anirudha here !! Hi  ALL THIS GOING WELL. CICD  WORKING VERY WELL  !!!! Bye ";
   }
 }

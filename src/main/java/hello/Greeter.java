51ca19e8ee217f2e0ca0cd3fe3b746338f11a5b0
anirudha_dak <123630876+anirudhadak2@users.noreply.github.com>
2024-07-16 11:14:44 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "    Hello Friends !! The webhook is working properly...  !!!! ";
+    return "    Hello Friends !! The webhook is working properly... !!!! Bye ";
   }
 }
